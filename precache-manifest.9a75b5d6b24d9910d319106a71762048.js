75e48574787428d5d6819733795324d09b579439
Larry <yuetao1998@gmail.com>
2020-07-10 01:19:44 -0500

    Updates

---
+++ precache-manifest.9a75b5d6b24d9910d319106a71762048.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee77417563a17ba78bdf83f08dcb06f8",
+    "url": "/jobsearch/index.html"
+  },
+  {
+    "revision": "59a37d12c1f1af315134",
+    "url": "/jobsearch/static/css/2.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "ce302bce5f154f57304c",
+    "url": "/jobsearch/static/css/main.54b5a34a.chunk.css"
+  },
+  {
+    "revision": "59a37d12c1f1af315134",
+    "url": "/jobsearch/static/js/2.23c4cb78.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/jobsearch/static/js/2.23c4cb78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce302bce5f154f57304c",
+    "url": "/jobsearch/static/js/main.694a0dfa.chunk.js"
+  },
+  {
+    "revision": "f4baa1314b33eda07757",
+    "url": "/jobsearch/static/js/runtime-main.cc7fb850.js"
+  }
+]);
